7e83a9f1cf46a3797b8d042c1e9d63925cd06fe6
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-02-18 12:21:00 +0100

    Update godzilla.py

--- godzilla.py
+++ godzilla.py
@@ -1,45 +1,97 @@
-# importamos las librerías
-import os, sys
+# importamos las librerías necesarias
 from colorama import Fore, Back
+from sys import argv
+import os, sys
 
-# configuramos los colores de colorama
-BLACK = '\033[30m'
-RED = '\033[31m'
-GREEN = '\033[32m'
-YELLOW = '\033[33m'
-BLUE = '\033[34m'
-MAGENTA = '\033[35m'
-CYAN = '\033[36m'
-WHITE = '\033[37m'
-RESET = '\033[39m'
+# declaramos los argumentos
+para1 = argv
+para2 = argv
 
-# hacemos el menú
-def menu():
+# hacemos las funciones principales
+def sherlock():
     os.system('clear')
     title = '''
-\033[31m   ▄██████▄   ▄██████▄  ████████▄  \033[34m ▄███████▄   ▄█   ▄█        ▄█          ▄████████ 
-\033[31m  ███    ███ ███    ███ ███   ▀███ \033[34m██▀     ▄██ ███  ███       ███         ███    ███ 
-\033[31m  ███    █▀  ███    ███ ███    ███ \033[34m      ▄███▀ ███▌ ███       ███         ███    ███ 
-\033[31m ▄███        ███    ███ ███    ███ \033[34m ▀█▀▄███▀▄▄ ███▌ ███       ███         ███    ███ 
-\033[31m▀▀███ ████▄  ███    ███ ███    ███ \033[34m  ▄███▀   ▀ ███▌ ███       ███       ▀███████████ 
-\033[31m  ███    ███ ███    ███ ███    ███ \033[34m▄███▀       ███  ███       ███         ███    ███ 
-\033[31m  ███    ███ ███    ███ ███   ▄███ \033[34m███▄     ▄█ ███  ███▌    ▄ ███▌    ▄   ███    ███ 
-\033[31m  ████████▀   ▀██████▀  ████████▀  \033[34m ▀████████▀ █▀   █████▄▄██ █████▄▄██   ███    █▀  
-\033[31m                                   \033[34m                 ▀         ▀          
-\033[31m[~] By ZombieGeek0: https://www.github.com/ZombieGeek0/GodZilla
-\033[31m[~] GitHub: https://github.com/ZombieGeek0           
+\033[31m   ▄████████    ▄█    █▄       ▄████████    ▄████████  ▄█        ▄██████▄   ▄████████    ▄█   ▄█▄ 
+\033[31m  ███    ███   ███    ███     ███    ███   ███    ███ ███       ███    ███ ███    ███   ███ ▄███▀ 
+\033[31m  ███    █▀    ███    ███     ███    █▀    ███    ███ ███       ███    ███ ███    █▀    ███▐██▀   
+\033[31m  ███         ▄███▄▄▄▄███▄▄  ▄███▄▄▄      ▄███▄▄▄▄██▀ ███       ███    ███ ███         ▄█████▀    
+\033[31m▀███████████ ▀▀███▀▀▀▀███▀  ▀▀███▀▀▀     ▀▀███▀▀▀▀▀   ███       ███    ███ ███        ▀▀█████▄    
+\033[31m         ███   ███    ███     ███    █▄  ▀███████████ ███       ███    ███ ███    █▄    ███▐██▄   
+\033[31m   ▄█    ███   ███    ███     ███    ███   ███    ███ ███▌    ▄ ███    ███ ███    ███   ███ ▀███▄ 
+\033[31m ▄████████▀    ███    █▀      ██████████   ███    ███ █████▄▄██  ▀██████▀  ████████▀    ███   ▀█▀ 
+\033[31m                                           ███    ███ ▀                                 ▀         
 '''
     print(title)
 
-    options = '''
-\033[34m[00]: Salir                                         \033[31m█\033[33m█\033[35m█\033[37m█\033[31m█\033[33m█\033[35m█\033[37m█
-\033[34m[01]: Volver al menú                                \033[32m█\033[34m█\033[36m█\033[30m█\033[32m█\033[34m█\033[36m█\033[30m█ 
-\033[34m[02]: Realizar una búsqueda con Sherlock
+    print(Fore.RED + Back.RESET + '========================================================================================')
+    os.system(Fore.BLUE + Back.RESET + f'cd sherlock && cd sherlock && python3 sherlock.py {para2[2]}')
+    print(Fore.RED + Back.RESET + '========================================================================================')
+    print(Fore.BLUE + Back.RESET + '[?] Proceso finalizado con éxito.')
+    print(Fore.RESET + Back.RESET)
+    sys.exit()
+
+
+def install():
+    os.system('clear')
+    title = '''
+\033[31m ▄█  ███▄▄▄▄      ▄████████     ███        ▄████████\033[34m  ▄█        ▄█          ▄████████    ▄████████ 
+\033[31m███  ███▀▀▀██▄   ███    ███ ▀█████████▄   ███    ███\033[34m ███       ███         ███    ███   ███    ███ 
+\033[31m███▌ ███   ███   ███    █▀     ▀███▀▀██   ███    ███\033[34m ███       ███         ███    █▀    ███    ███ 
+\033[31m███▌ ███   ███   ███            ███   ▀   ███    ███\033[34m ███       ███        ▄███▄▄▄      ▄███▄▄▄▄██▀ 
+\033[31m███▌ ███   ███ ▀███████████     ███     ▀███████████\033[34m ███       ███       ▀▀███▀▀▀     ▀▀███▀▀▀▀▀   
+\033[31m███  ███   ███          ███     ███       ███    ███\033[34m ███       ███         ███    █▄  ▀███████████ 
+\033[31m███  ███   ███    ▄█    ███     ███       ███    ███\033[34m ███▌    ▄ ███▌    ▄   ███    ███   ███    ███ 
+\033[31m█▀    ▀█   █▀   ▄████████▀     ▄████▀     ███    █▀ \033[34m █████▄▄██ █████▄▄██   ██████████   ███    ███ 
+\033[31m                                                    \033[34m ▀         ▀                        ███    ███ 
 '''
-    print(options)
+    print(title)
+    print(Fore.RED + Back.RESET + '========================================================================================')
+    os.system(Fore.BLUE + Back.RESET + f'sudo su && sudo apt-get update && apt-get install git -y && sudo apt-get install python3 && pip3 install colorama && sudo apt-get install python3-pip && git clone https://github.com/sherlock-project/sherlock')
+    print(Fore.RED + Back.RESET + '========================================================================================')
+    print(Fore.BLUE + Back.RESET + '[?] Proceso finalizado con éxito.')
+    print(Fore.RESET + Back.RESET)
+    sys.exit()
 
-    choice = input(Fore.RED + Back.RESET + '[?] -->  ')
+def credits():
+    os.system('clear')
+    title = '''
+\033[31m ▄████████    ▄████████    ▄████████\033[34m ████████▄   ▄█      ███        ▄████████ 
+\033[31m███    ███   ███    ███   ███    ███\033[34m ███   ▀███ ███  ▀█████████▄   ███    ███ 
+\033[31m███    █▀    ███    ███   ███    █▀ \033[34m ███    ███ ███▌    ▀███▀▀██   ███    █▀  
+\033[31m███         ▄███▄▄▄▄██▀  ▄███▄▄▄    \033[34m ███    ███ ███▌     ███   ▀   ███        
+\033[31m███        ▀▀███▀▀▀▀▀   ▀▀███▀▀▀    \033[34m ███    ███ ███▌     ███     ▀███████████ 
+\033[31m███    █▄  ▀███████████   ███    █▄ \033[34m ███    ███ ███      ███              ███ 
+\033[31m███    ███   ███    ███   ███    ███\033[34m ███   ▄███ ███      ███        ▄█    ███ 
+\033[31m████████▀    ███    ███   ██████████\033[34m ████████▀  █▀      ▄████▀    ▄████████▀  
+\033[31m             ███    ███             \033[34m                                          
+'''
+    print(title)
+    print(Fore.RED + Back.RESET + '========================================================================================')
+    options = '''
+[?] Este proyecto ha sido creado por ZombieGeek0.
+[?] GitHub: https://www.github.com/ZombieGeek0.
+[?] GitHub del proyecto: https://github.com/ZombieGeek0/GodZilla.
+'''
+    print(Fore.BLUE + Back.RESET + options)
+    print(Fore.RED + Back.RESET + '========================================================================================')
+    print(Fore.BLUE + Back.RESET + '[?] Proceso finalizado con éxito.')
+    print(Fore.RESET + Back.RESET)
+    sys.exit()
 
+# hacemos el menú
+def menu():
+    if para1[1] == '--install':
+        install()
 
+    elif para1[1] == '--sherlock':
+        sherlock()
+
+    elif para1[1] == '--credits':
+        credits()
+
+    else:
+        print(Fore.RED + Back.RESET + '\n[?] Error: Revisa si has incluido todos los parámetros necesarios en la petición.')
+        print(Fore.RESET + Back.RESET)
+        sys.exit()
 
 menu()
