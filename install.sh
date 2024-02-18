d3f29e41dcc6057f136b62cf057e912b2277ce3d
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-02-18 16:34:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ clear
 echo ====================================================================================================
 echo [?] Instalando...
 echo ====================================================================================================
-sudo su && sudo apt-get update && apt-get install git -y && sudo apt-get install python3 && pip3 install colorama && sudo apt-get install python3-pip && git clone https://github.com/sherlock-project/sherlock
+sudo su && sudo apt-get update && apt-get install git -y && sudo apt-get install python3 && pip3 install webbrowser && pip3 install colorama && sudo apt-get install python3-pip && git clone https://github.com/sherlock-project/sherlock
 echo ====================================================================================================
 echo [?] Requerimientos instalados.
