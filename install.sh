8f5a88cf71ca1118aad1bdc2790cd6e4dbd2e84f
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-02-22 10:51:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ clear
 echo ====================================================================================================
 echo [?] Instalando...
 echo ====================================================================================================
-sudo su && sudo apt-get update && apt-get install git -y && sudo apt-get install python3 && pip3 install webbrowser && pip3 install colorama && sudo apt-get install python3-pip && git clone https://github.com/sherlock-project/sherlock && git clone https://github.com/sqlmapproject/sqlmap
+sudo apt-get update && apt-get install git -y && sudo apt-get install python3 && pip3 install webbrowser && pip3 install colorama && sudo apt-get install python3-pip && git clone https://github.com/sherlock-project/sherlock && git clone https://github.com/sqlmapproject/sqlmap
 echo ====================================================================================================
 echo [?] Requerimientos instalados.
